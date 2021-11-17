4913af296a609d8035d0e44431131621cd7c8f8b
venkat <venkatg@samisen.io>
2021-11-17 17:36:41 +0530

    created Spinner

---
+++ client/src/components/Spinner.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="loading loading--full-height"></div>
+</template>
+
+<script>
+export default {
+    
+};
+</script>
+
+<style>
+/* Style #1 */
+.loading {
+  display: flex;
+  justify-content: center;
+}
+
+.loading--full-height {
+  align-items: center;
+  height: 100%;
+}
+
+.loading::after {
+  content: "";
+  width: 50px;
+  height: 50px;
+  border: 10px solid #dddddd;
+  border-top-color: #009579;
+  border-radius: 50%;
+  transform: rotate(0.16turn);
+  animation: loading 1s ease infinite;
+}
+
+@keyframes loading {
+  /* Safari support */
+  from {
+    transform: rotate(0turn);
+  }
+
+  to {
+    transform: rotate(1turn);
+  }
+}
+</style>
