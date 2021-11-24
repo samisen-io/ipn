b720fba6c62e2b75ba9c2eb6bd0e595d62a2eb1f
venkat <venkatg@samisen.io>
2021-11-24 15:31:27 +0530

    updated file

--- client/src/components/ResetPassword.vue
+++ client/src/components/ResetPassword.vue
@@ -1,5 +1,40 @@
 <template>
-  <h1>ResetPassword</h1>
+  <div>
+    <div class="container">
+      <h1 class="forgotHeading">ResetPassword</h1>
+      <div class="d-flex justify-content-center">
+        <div class="ResetPassword">
+          <div class="input-group mb-3">
+            <span class="input-group-text"
+              ><i class="fas fa-key fa-2x"></i
+            ></span>
+            <input
+              type="password"
+              class="form-control"
+              placeholder="password"
+              aria-label="Location"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="input-group mb-3">
+            <span class="input-group-text"
+              ><i class="fas fa-key fa-2x"></i
+            ></span>
+            <input
+              type="password"
+              class="form-control"
+              placeholder="password"
+              aria-label="Location"
+              aria-describedby="basic-addon1"
+            />
+          </div>
+          <div class="submitbutton">
+            <button type="button" class="btn btn-primary">Reset</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,4 +42,26 @@ export default {};
 </script>
 
 <style>
+.ResetPassword {
+  margin-top: 100px;
+  height: 200px;
+  width: 50%;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  padding: 10px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -webkit-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  -moz-box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2),
+    0 6px 20px 0 rgba(0, 0, 0, 0.19);
+  border-radius: 5px;
+}
+.submitbutton {
+  text-align: right;
+}
+.forgotHeading {
+  text-align: center;
+}
 </style>
