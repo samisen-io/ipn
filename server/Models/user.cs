2b172eeb2c4f0b056c635405d834348b94b2fd2f
venkat <venkatg@samisen.io>
2021-11-24 14:50:36 +0530

    Added validations for user Model

--- server/Models/user.cs
+++ server/Models/user.cs
@@ -1,16 +1,25 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace server.Models;
 
 public class User
 {
-  public long? id { get; set; }
-  public string firstName { get; set; }
-  public string middleName { get; set; }
-  public string lastName { get; set; }
-  public string mobile { get; set; }
-  public string email { get; set; }
-  public string passwordHash { get; set; }
-  public DateTime registeredAt { get; set; }
-  public DateTime lastLogin { get; set; }
-  public string profile { get; set; }
+    public long? id { get; set; }
+    [Required]
+    public string firstName { get; set; }
+    public string middleName { get; set; }
+    [Required]
+    public string lastName { get; set; }
+    [Required]
+    [Required]
+    public string mobile { get; set; }
+    [Required]
+    public string email { get; set; }
+    [Required]
+    public string passwordHash { get; set; }
+    [Required]
+    public DateTime registeredAt { get; set; }
+    public DateTime lastLogin { get; set; }
+    public string profile { get; set; }
 }
 
